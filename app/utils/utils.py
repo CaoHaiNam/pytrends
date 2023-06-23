a59f5184de69f8cbae4028f1562d092f2315509c
CaoHaiNam <namch.hust@gmail.com>
2023-06-23 14:02:23 +0700

    add keywords ranking function

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,15 +1,27 @@
 import pytrends
 from urllib.parse import quote
 import pandas as pd 
+from typing import List
 from pytrends.request import TrendReq
+import app.utils.config as config
 
 pytrend_ins = TrendReq(hl='vi', 
                        tz=420, 
                        geo='VN',
                        proxies=[])
 
-def keyword_suggestion(keyword):
+def keyword_suggestion(keyword: str):
     suggestions_dict = pytrend_ins.suggestions(keyword=keyword)
     return [i['title'] for i in suggestions_dict]
 
 
+def keyword_ranking(keywords: List[str], timeframe: str = 'day'):
+    pytrend_ins.build_payload(keywords, 
+                          cat=0, 
+                          timeframe=config.TIMEFRAME_CONFIG[timeframe], 
+                          geo='VN', 
+                          gprop='',
+                          )
+    df = pytrend_ins.interest_over_time()
+    df = df.drop(columns=['isPartial'])
+    return df.sum().to_dict()
